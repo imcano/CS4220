6781ecde46413362793ad8cf5b99dc8282116215
Cano <icano3@calstatela.edu>
2019-02-04 00:08:21 -0800

    P02 done

--- W02H01.js
+++ W02H01.js
@@ -1,14 +1,30 @@
+// P01
+
 const replaceLetters = (str, arr) => {
   let newStr = '';
 
   [...str].forEach(char => newStr += arr.includes(char) ? '-': char);
 
-  console.log(newStr);
+  return newStr;
 }
 
-const arr = ['a', 'e', 'i', 'o', 'u' ];
-replaceLetters('node.js', arr);
+const arr1 = ['a', 'e', 'i', 'o', 'u' ];
+replaceLetters('node.js', arr1);
+
+// P02
 
 const sumArray = (arr) => {
-  
+  let sum = 0;
+
+  arr.forEach(element => sum += isNaN(parseInt(element)) ? 0 : parseInt(element));
+
+  console.log(sum);
 }
+
+const arr2 = [5, 2, 'a', 4, '7', true, 'b', 'c', 7, '8', false]
+sumArray(arr2)
+
+// P03
+
+
+
