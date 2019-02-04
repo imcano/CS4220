3f0ee7bc881af30c1583e7e954d6c87a4f524cee
Cano <icano3@calstatela.edu>
2019-02-04 03:17:19 -0800

    P03 done

--- W02H01.js
+++ W02H01.js
@@ -1,11 +1,12 @@
 // P01
 
 const replaceLetters = (str, arr) => {
-  let newStr = '';
+  // let newStr = '';
+  // [...str].forEach(char => newStr += arr.includes(char) ? '-': char);
 
-  [...str].forEach(char => newStr += arr.includes(char) ? '-': char);
+  const newStr = [...str].reduce((str, char) => str + (arr.includes(char) ? '-': char));
 
-  return newStr;
+  console.log(newStr);
 }
 
 const arr1 = ['a', 'e', 'i', 'o', 'u' ];
@@ -14,9 +15,10 @@ replaceLetters('node.js', arr1);
 // P02
 
 const sumArray = (arr) => {
-  let sum = 0;
-
-  arr.forEach(element => sum += isNaN(parseInt(element)) ? 0 : parseInt(element));
+  // let sum = 0;
+  // arr.forEach(element => sum += isNaN(parseInt(element)) ? 0 : parseInt(element));
+  
+  const sum = arr.reduce((acc, e) => acc + (isNaN(parseInt(e)) ? 0 : parseInt(e)));
 
   console.log(sum);
 }
@@ -26,5 +28,35 @@ sumArray(arr2)
 
 // P03
 
+const countingWords = (arr) => {
+  let wordCount = {};
+  arr.forEach(word => wordCount.hasOwnProperty(word) ? wordCount[word]++ : wordCount[word] = 1 );
 
+  console.log(wordCount);
+}
 
+const arr3 = ['hi', 'hi', 'hello', 'world', 'hello', 'hi' , 'greetings']
+countingWords(arr3);
+
+// P04
+
+const createAnimals = (arr) => {
+  x = arr[0].reduce((acc, curr) => Object.assign(acc, curr), {});
+
+  console.log(arr[1]);
+}
+
+const arr4 =
+       [
+           [
+               { property: 'name', assign: 'Garfield'},
+               { property: 'owner', assign: 'Jon Arbuckle'},
+               { property: 'type', assign: 'cat' }
+           ],
+           [
+               { property: 'name',assign: 'Snoopy' },
+               { property: 'owner',assign: 'Charlie Brown' },
+               { property: 'type',assign: 'dog' }
+           ]
+       ]
+createAnimals(arr4);
